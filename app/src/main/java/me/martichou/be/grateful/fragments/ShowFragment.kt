19a0ade26fa1378fd845c670ca6a61f087f14021
Martichou <martichou.andre@gmail.com>
2018-12-03 19:23:07 +0100

    design: Add the date of the note - will need to design this pattern

--- app/src/main/java/me/martichou/be/grateful/fragments/ShowFragment.kt
+++ app/src/main/java/me/martichou/be/grateful/fragments/ShowFragment.kt
@@ -2,18 +2,17 @@ package me.martichou.be.grateful.fragments
 
 import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.interpolator.view.animation.FastOutSlowInInterpolator
 import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
-import com.yarolegovich.lovelydialog.LovelyStandardDialog
-import me.martichou.be.grateful.R
 import me.martichou.be.grateful.databinding.ShowFragmentBinding
 import me.martichou.be.grateful.utilities.InjectorUtils
 import me.martichou.be.grateful.utilities.MoveViews
@@ -43,38 +42,7 @@ class ShowFragment : Fragment() {
             interpolator = FastOutSlowInInterpolator()
         }
 
-        setHasOptionsMenu(true)
         return binding.root
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
-        inflater?.inflate(R.menu.show, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.edit_note -> {
-                true
-            }
-            R.id.delete_note -> {
-
-                LovelyStandardDialog(context, LovelyStandardDialog.ButtonLayout.HORIZONTAL)
-                        .setTopColorRes(R.color.red)
-                        .setButtonsColorRes(R.color.black)
-                        .setIcon(R.drawable.ic_delete_white)
-                        .setTitle("Take a second...")
-                        .setMessage("Are you sure you really want to delete this note? After that you won't be able to recover it.")
-                        .setPositiveButton(android.R.string.yes) {
-                            this.findNavController().popBackStack()
-                            viewModel.deleteNote(noteId)
-                        }
-                        .setNegativeButton(android.R.string.cancel, null)
-                        .show()
-
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 
     private val imageListener = object : RequestListener<Drawable> {
