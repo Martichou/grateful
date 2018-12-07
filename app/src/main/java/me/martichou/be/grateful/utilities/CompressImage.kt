c6030c8f102e0b779965a5feee228aa989490c86
Martichou <martichou.andre@gmail.com>
2018-12-07 14:25:51 +0100

    compress: Use a supervisorjob to handle crash

--- app/src/main/java/me/martichou/be/grateful/utilities/CompressImage.kt
+++ app/src/main/java/me/martichou/be/grateful/utilities/CompressImage.kt
@@ -6,30 +6,25 @@ import android.graphics.BitmapFactory
 import android.util.Log
 import androidx.appcompat.widget.AppCompatImageButton
 import androidx.core.content.ContextCompat
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
 import me.martichou.be.grateful.R
 import me.martichou.be.grateful.viewmodels.AddViewModel
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
-import kotlin.coroutines.CoroutineContext
 import kotlin.system.exitProcess
 
-class CompressImage(context: Context, viewModel: AddViewModel, file: File, add_btn: AppCompatImageButton) : CoroutineScope {
+class CompressImage(context: Context, viewModel: AddViewModel, file: File, add_btn: AppCompatImageButton) {
 
-    private val job = Job()
+    private val job = SupervisorJob()
 
-    override val coroutineContext: CoroutineContext = job + Main
+    private val scope = CoroutineScope(Dispatchers.Default + job)
 
     private val storageDir = context.getDir("imgForNotes", Context.MODE_PRIVATE)
 
     init {
-        launch {
+        scope.launch {
             if (if (!storageDir.exists()) { storageDir.mkdirs() } else { true }) {
                 val imageFile = File(storageDir, viewModel.randomImageName)
                 if (imageFile.exists()) {
@@ -41,9 +36,7 @@ class CompressImage(context: Context, viewModel: AddViewModel, file: File, add_b
                 }
                 val fos: FileOutputStream = async(IO) { FileOutputStream(imageFile) }.await()
                 try {
-                    async(IO) {
-                        BitmapFactory.decodeFile(file.absolutePath).compress(Bitmap.CompressFormat.JPEG, 75, fos)
-                    }.await()
+                    async(IO) {BitmapFactory.decodeFile(file.absolutePath).compress(Bitmap.CompressFormat.JPEG, 75, fos)}.await()
                 } catch (e: Exception) {
                     e.printStackTrace()
                 } finally {
