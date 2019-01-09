142a542549add737cd8d34a67539376ef761f2e3
Martichou <martichou.andre@gmail.com>
2019-01-09 22:16:04 +0100

    reset database version to 1

--- app/src/main/java/me/martichou/be/grateful/data/AppDatabase.kt
+++ app/src/main/java/me/martichou/be/grateful/data/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Notes::class], version = 2, exportSchema = false)
+@Database(entities = [Notes::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun notesDao(): NotesDao
 
