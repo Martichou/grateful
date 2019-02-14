9250e5c15c16a1edcb7a2c65ab10b690dbede708
Martichou <martichou.andre@gmail.com>
2019-02-14 22:07:44 +0100

    fix shared elements transition from main to details thanks to constraint
    - Used constraintlayout in the recyclerview_homeitem.xml with aspect ratio
    - Getting the size of the image and compute the aspect ration to apply it in bind fun

--- app/src/main/java/me/martichou/be/grateful/recyclerView/NotesAdapter.kt
+++ app/src/main/java/me/martichou/be/grateful/recyclerView/NotesAdapter.kt
@@ -1,16 +1,25 @@
 package me.martichou.be.grateful.recyclerView
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.SimpleTarget
 import me.martichou.be.grateful.data.Notes
 import me.martichou.be.grateful.databinding.RecyclerviewHomeitemBinding
 import me.martichou.be.grateful.fragments.HomeMainFragmentDirections
+import me.martichou.be.grateful.utilities.Size2
+import timber.log.Timber
+import java.io.File
 
 class NotesAdapter : ListAdapter<Notes, NotesAdapter.ViewHolder>(NotesDiffCallback()) {
 
@@ -42,12 +51,37 @@ class NotesAdapter : ListAdapter<Notes, NotesAdapter.ViewHolder>(NotesDiffCallba
     }
 
     class ViewHolder(private val binding: RecyclerviewHomeitemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        private val set = ConstraintSet()
+
         fun bind(listener: OnNoteItemClickListener, item: Notes) {
+            Glide.with(itemView.context)
+                .`as`(Size2::class.java)
+                .apply(sizeOptions)
+                .load(File(itemView.context.getDir("imgForNotes", Context.MODE_PRIVATE), item.image))
+                .into(object : SimpleTarget<Size2>() {
+                    override fun onResourceReady(resource: Size2, transition: com.bumptech.glide.request.transition.Transition<in Size2>?) {
+                        Timber.i("Size: $resource")
+                        with(set) {
+                            val posterRatio = String.format("%d:%d", resource.width, resource.height)
+                            clone(binding.parentContsraint)
+                            setDimensionRatio(binding.showImageNote.id, posterRatio)
+                            applyTo(binding.parentContsraint)
+                        }
+                    }
+                })
+
             binding.apply {
                 clickListener = listener
                 note = item
                 executePendingBindings()
             }
+        }
+
+        private val sizeOptions by lazy {
+            RequestOptions()
+                .skipMemoryCache(true)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
         }
     }
 
--- app/src/main/java/me/martichou/be/grateful/utilities/BindingAdapters.kt
+++ app/src/main/java/me/martichou/be/grateful/utilities/BindingAdapters.kt
@@ -5,10 +5,13 @@ import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -17,7 +20,7 @@ import java.util.Locale
  * Bind the app:imageFromFile="imgName" from xml
  */
 @BindingAdapter("imageFromFile", "requestListener", requireAll = false)
-fun imageFromFile(view: ImageView, imageUrl: String?, listener: RequestListener<Drawable>?) {
+fun imageFromFile(view: AppCompatImageView, imageUrl: String?, listener: RequestListener<Drawable>?) {
     if (!imageUrl.isNullOrEmpty()) {
         GlideApp.with(view.context)
                 .load(File(view.context.getDir("imgForNotes", Context.MODE_PRIVATE), imageUrl))
