091b67e8a099156f0f6c2aa424a6a9147d201519
Martichou <martichou.andre@gmail.com>
2019-04-30 22:18:02 +0200

    common: remove unused fun

--- app/src/main/java/me/martichou/be/grateful/utils/Common.kt
+++ app/src/main/java/me/martichou/be/grateful/utils/Common.kt
@@ -1,27 +1,12 @@
 package me.martichou.be.grateful.utils
 
-import android.Manifest
 import android.content.Context
-import android.content.pm.PackageManager
 import android.util.DisplayMetrics
 import android.view.WindowManager
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.random.Random
-
-/**
- * Called to setup needed permission for Grateful
- */
-fun setupPermissions(activity: AppCompatActivity, context: Context) {
-    val permission = ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-    if (permission != PackageManager.PERMISSION_GRANTED) {
-        ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 101)
-    }
-}
 
 /**
  * @return the current time time in millis
