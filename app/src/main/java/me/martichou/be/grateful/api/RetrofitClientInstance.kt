6ba6a01fcd1a754006f2d722666aefbbefa11068
Martichou <martichou.andre@gmail.com>
2019-05-02 16:30:53 +0200

    sync: update server to local and fix retreive image

--- app/src/main/java/me/martichou/be/grateful/api/RetrofitClientInstance.kt
+++ app/src/main/java/me/martichou/be/grateful/api/RetrofitClientInstance.kt
@@ -9,7 +9,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 object RetrofitClientInstance {
 
     private var retrofit: Retrofit? = null
-    private const val BASE_URL = "https://grateful-restful.herokuapp.com/"
+    private const val BASE_URL = "http://192.168.1.27:8080/"
 
     val retrofitInstance: Retrofit?
         get() {
