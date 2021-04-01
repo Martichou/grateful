6bcfcb5d789b0fe812ba91981e30c375dd25742e
Martichou <martichou.andre@gmail.com>
2021-04-01 19:44:08 +0200

    Target SDK30

--- app/src/main/java/me/martichou/be/grateful/di/AppInjector.kt
+++ app/src/main/java/me/martichou/be/grateful/di/AppInjector.kt
@@ -29,7 +29,7 @@ object AppInjector {
             override fun onActivityResumed(activity: Activity) {}
             override fun onActivityPaused(activity: Activity) {}
             override fun onActivityStopped(activity: Activity) {}
-            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle?) {}
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
             override fun onActivityDestroyed(activity: Activity) {}
         })
     }
